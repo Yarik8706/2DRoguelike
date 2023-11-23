9d9357f1224b51fbb1775bdf5b438eae9fbba0ff
Geralexm <115567909+Geralexm@users.noreply.github.com>
2023-11-23 18:21:37 +0300

    Added floor and lamp with animation

--- Assets/Scripts/SeveralVariants.cs
+++ Assets/Scripts/SeveralVariants.cs
@@ -8,7 +8,7 @@ public class SeveralVariants : MonoBehaviour
     public Sprite[] sprites;
     public bool isTurnX;
     public bool isTurnY;
-    
+
     private void Start()
     {
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
@@ -17,15 +17,21 @@ public class SeveralVariants : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
-        
+
         if (isTurnY && Random.Range(0, 2) == 1)
         {
             spriteRenderer.flipX = true;
         }
-        
+
         // code
-        //Instantiate(lamp, transform.position, Quaternion.identity);
-        
+        if (lamp != null) 
+        {
+            if (Random.Range(0, 6) == 1)
+            {
+                Instantiate(lamp, transform.position, Quaternion.identity);
+            }
+        }
+
         //TODO: сделать пол и его рандомную смену текстуры
         //TODO: сделать лампу с анимацией 
         //TODO: сделать появление лампы на стене 
